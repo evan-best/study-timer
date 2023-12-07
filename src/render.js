37130d426a334f1ec404a227e77c0b4b1e038f0c
evan-best <117332327+evan-best@users.noreply.github.com>
2023-12-07 18:17:20 -0330

    Added audio for when timer ends

--- src/render.js
+++ src/render.js
@@ -5,12 +5,17 @@ const clearBtn = document.getElementById("clearBtn");
 const pomodoroBtn = document.getElementById("pomodoroBtn");
 const shortBreakBtn = document.getElementById("shortBreakBtn");
 const longBreakBtn = document.getElementById("longBreakBtn");
+
+// Initialize timer components
 const timer = document.getElementById("timer");
+const timerSound = new Audio('audio/marimba-for-smartphone.mp3')
 let countdownInterval;
 let remainingTime = { minutes: 25, seconds: 0 };
 
 function setTimerValue(minutes, seconds) {
-    timer.textContent = formatTime(minutes, seconds);
+    timer.textContent = formatTime(minutes, seconds)
+    remainingTime.minutes = minutes;
+    remainingTime.seconds = seconds;
 }
 
 function formatTime(minutes, seconds) {
@@ -29,6 +34,11 @@ function startCountdown() {
             remainingTime.seconds = 59;
         } else {
             clearInterval(countdownInterval);
+            
+            // Play audio at end of timer
+            if (remainingTime.minutes === 0 && remainingTime.seconds === 0){
+                playTimerSound();
+            }
         }
 
         setTimerValue(remainingTime.minutes, remainingTime.seconds);
@@ -67,6 +77,12 @@ function longBreakBtnOnClick() {
     setTimerValue(remainingTime.minutes, remainingTime.seconds);
 }
 
+
+function playTimerSound() {
+        timerSound.play();
+}
+
+// Event listeners for buttons
 pomodoroBtn.addEventListener('click', pomodoroBtnOnClick);
 shortBreakBtn.addEventListener('click', shortBreakBtnOnClick);
 longBreakBtn.addEventListener('click', longBreakBtnOnClick);
